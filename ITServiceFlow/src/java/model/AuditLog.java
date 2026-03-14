ce618894172ad60e25069e6a24df9c0e7394c152
HoangNV <nguyenhoang852003@gmail.com>
2026-03-15 00:00:33 +0700

    System Audit Logs

---
+++ ITServiceFlow/src/java/model/AuditLog.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author DELL
+ */
+public class AuditLog {
+    private int id;
+    private int userId;
+    private String userName; // For display
+    private String action;
+    private String screen;
+    private String dataBefore;
+    private String dataAfter;
+    private Timestamp createdAt;
+
+    private String entity;
+    private Integer entityId;
+
+    public AuditLog() {
+    }
+
+    public AuditLog(int userId, String action, String screen, String dataBefore, String dataAfter) {
+        this.userId = userId;
+        this.action = action;
+        this.screen = screen;
+        this.dataBefore = dataBefore;
+        this.dataAfter = dataAfter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getScreen() {
+        return screen;
+    }
+
+    public void setScreen(String screen) {
+        this.screen = screen;
+    }
+
+    public String getDataBefore() {
+        return dataBefore;
+    }
+
+    public void setDataBefore(String dataBefore) {
+        this.dataBefore = dataBefore;
+    }
+
+    public String getDataAfter() {
+        return dataAfter;
+    }
+
+    public void setDataAfter(String dataAfter) {
+        this.dataAfter = dataAfter;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public Integer getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(Integer entityId) {
+        this.entityId = entityId;
+    }
+}
